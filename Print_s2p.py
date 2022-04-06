542ba378cc2d45790cd4691471c79c27878266d9
jgiljumTL <jgiljum@thorlabs.com>
2022-04-06 16:47:59 -0400

    Updated code to include 2 for loops for asking operator for file selection

--- Print_s2p.py
+++ Print_s2p.py
@@ -1,30 +1,26 @@
 
-
+import os
 import tkinter as tk
 import matplotlib.pyplot as plt
 from skrf import Network
 from tkinter import filedialog
 
 
-files =["RX25AF_U01283_VNA_N_BWL=0,00_BWH=0,00_GC=1,50_00.s2p","RX25AF_U00840_VNA_N_BWL=2,70_BWH=2,70_GC=2,20_00.s2p"]
+files =[]
 
-#t = Network("RX25AF_U01283_VNA_N_BWL=0,00_BWH=0,00_GC=1,50_00.s2p")
-#r = Network("RX25AF_U00840_VNA_N_BWL=2,70_BWH=2,70_GC=2,20_00.s2p")
 
 root = tk.Tk()
 root.withdraw()
 
+for y in files: #loop for adding file paths
+    file_path = filedialog.askopenfilename() #opens tkinter dialog box for operator to select the .s2p file
+    files.append(file_path) #add the .s2p file path to the files list
+
+for x in files: #loop for pulling pulling items from files list
+    h = Network(os.path.basename(x)) #pulls just the file name from the entire file path stored in the files list and converts it to a network object
+    h.s21.plot_s_db() #plots each network object (s21_dB)
 
 
-file_path = filedialog.askopenfilename()
 
-for x in files:
-    h = Network(x)
-    h.s21.plot_s_db()
-
-
-#t.s21.plot_s_db() #Plot only s21 parameter
-#r.s21.plot_s_db()
-
-plt.show()
+plt.show() #plot all of the network object from above.
 

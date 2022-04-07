df6180d3b7be73331bc5e8d3126493017e4dfcd3
jgiljumTL <jgiljum@thorlabs.com>
2022-04-07 11:01:29 -0400

    - Updated first for loop to a while loop - Removed "os.path.basename() from within "h = Network()" line (Now it pulls all of the file path instead of just the .s2p,this fixed the error. - added if statement to check status of "run" variable. - added if statement to handle BLANK SPACE in "Files" list - RESULT: Code can now allow user to select multiple files one-by-one in the pop-up window.

--- Print_s2p.py
+++ Print_s2p.py
@@ -4,21 +4,34 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 from skrf import Network
 from tkinter import filedialog
+from tkinter import *
 
 
 files =[]
-
+run = 1
 
 root = tk.Tk()
 root.withdraw()
 
-for y in files: #loop for adding file paths
+while run == 1: #loop for adding file paths
     file_path = filedialog.askopenfilename() #opens tkinter dialog box for operator to select the .s2p file
+    #my_btn = Button(root, text="Close", command=exit)
+
+    if file_path == "":
+        run = 0
+    else:
+        run = 1
+
     files.append(file_path) #add the .s2p file path to the files list
 
+
+
 for x in files: #loop for pulling pulling items from files list
-    h = Network(os.path.basename(x)) #pulls just the file name from the entire file path stored in the files list and converts it to a network object
-    h.s21.plot_s_db() #plots each network object (s21_dB)
+    if x == "":
+        break
+    else:
+        h = Network(x) #pulls just the file name from the entire file path stored in the files list and converts it to a network object
+        h.s21.plot_s_db() #plots each network object (s21_dB)
 
 
 
